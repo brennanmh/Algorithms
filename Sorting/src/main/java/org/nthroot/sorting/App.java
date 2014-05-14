be4012e02b2783e7bf1981c7955a388c2bf3ec41
Brennan Hildebrand <brenmh@med.umich.edu>
2014-05-14 16:38:56 -0400

    Added merge sorting of Comparable[]. Removed leading spaces from 32Kints.txt

--- Sorting/src/main/java/org/nthroot/sorting/App.java
+++ Sorting/src/main/java/org/nthroot/sorting/App.java
@@ -7,25 +7,27 @@ import java.io.BufferedReader;
 
 public class App {
 
-  public static void main(String [] args) {
+    public static void main(String[] args) {
 
-    System.out.println("Welcome to Sorting!");
+        System.out.println("Welcome to Sorting!");
 
-    if (args.length < 1) {
-      System.err.println("Please specify test file");
-      System.exit(1);
-    }
+        if (args.length < 1) {
+            System.err.println("Please specify test file");
+            System.exit(1);
+        }
 
-    String inputFile = args[0];
+        String inputFile = args[0];
 
-    int[] inputData = null;
-    try {
-      inputData = loadInts(inputFile);
-    } catch (Exception e) {
-      System.err.println("Failed to load input data");
-      System.err.println(e.getMessage());
-      System.exit(1);
-    }
+        //int[] inputData = null;
+        Integer[] inputData = null;
+        try {
+            //inputData = loadInts(inputFile);
+            inputData = loadIntegers(inputFile);
+        } catch (Exception e) {
+            System.err.println("Failed to load input data");
+            System.err.println(e.getMessage());
+            System.exit(1);
+        }
 
     /*
     for (int i = 0 ; i < inputData.length ; i++) {
@@ -33,13 +35,14 @@ public class App {
     }
     */
 
-    if (MergeSort.isSorted(inputData)) {
-      System.out.println("Array is sorted");
-    } else {
-      System.out.println("Array is not sorted");
-    }
+        if (MergeSort.isSorted(inputData)) {
+            System.out.println("Array is sorted");
+        } else {
+            System.out.println("Array is not sorted");
+        }
 
-    int[] outputData = MergeSort.sort(inputData);
+        //int[] outputData = MergeSort.sort(inputData);
+        Comparable[] outputData = MergeSort.sort(inputData);
 
     /*
     for (int i = 0 ; i < outputData.length ; i++) {
@@ -47,41 +50,65 @@ public class App {
     }
     */
 
-    if (MergeSort.isSorted(outputData)) {
-      System.out.println("Array is sorted");
-    } else {
-      System.out.println("Array is not sorted");
+        if (MergeSort.isSorted(outputData)) {
+            System.out.println("Array is sorted");
+        } else {
+            System.out.println("Array is not sorted");
+        }
+
     }
 
-  }
+    private static int[] loadInts(String filename) throws Exception {
 
-  private static int[] loadInts(String filename) throws Exception {
+        ArrayList arr = new ArrayList<Integer>();
+        File file = new File(filename);
+        BufferedReader reader = null;
 
-    ArrayList arr = new ArrayList<Integer>();
-    File file = new File(filename);
-    BufferedReader reader = null;
+        reader = new BufferedReader(new FileReader(file));
+        String intext = null;
 
-    reader = new BufferedReader(new FileReader(file));
-    String intext = null;
+        try {
+            while ((intext = reader.readLine()) != null) {
+                arr.add(Integer.parseInt(intext));
+            }
+        } catch (Exception e) {
+            System.err.println("Failed to parse int");
+            throw (e);
+        }
 
-    try {
-      while ((intext = reader.readLine()) != null) {
-        arr.add(Integer.parseInt(intext));
-      }
-    } catch (Exception e) {
-      System.err.println("Failed to parse int");
-      throw(e);
+        reader.close();
+
+        int result[] = new int[arr.size()];
+        for (int i = 0; i < arr.size(); i++) {
+            Integer val = (Integer) arr.get(i);
+            result[i] = val.intValue();
+        }
+
+        return result;
     }
 
-    reader.close();
+    private static Integer[] loadIntegers(String filename) throws Exception {
 
-    int result[] = new int[arr.size()];
-    for (int i = 0 ; i < arr.size() ; i++) {
-      Integer val = (Integer)arr.get(i);
-      result[i] = val.intValue();
+        ArrayList arr = new ArrayList<Integer>();
+        File file = new File(filename);
+        BufferedReader reader = null;
+
+        reader = new BufferedReader(new FileReader(file));
+        String intext = null;
+
+        try {
+            while ((intext = reader.readLine()) != null) {
+                arr.add(Integer.parseInt(intext));
+            }
+        } catch (Exception e) {
+            System.err.println("Failed to parse int");
+            throw (e);
+        }
+
+        reader.close();
+
+        Integer[] result = (Integer[])arr.toArray(new Integer[arr.size()]);
+
+        return result;
     }
-
-    return result;
-  }
-
 }
--- Sorting/src/main/java/org/nthroot/sorting/MergeSort.java
+++ Sorting/src/main/java/org/nthroot/sorting/MergeSort.java
@@ -1,58 +1,63 @@
 
-
 package org.nthroot.sorting;
 
 
 public class MergeSort {
 
-  public static void printArr(int[] arr) {
-    System.out.print("|");
-    for (int i = 0 ; i < arr.length ; i++) {
-      System.out.print(String.format("%d|", arr[i]));
-    }
-    System.out.println();
-  }
 
-  // with int arrays
-  public static int[] sort(int[] arr) {
-
-//    System.out.println("Sorting");
-
-    if (arr.length <= 1) {
-      return arr;
+    /////////////////////////////////////////
+    //
+    // int array sort
+    //
+    /////////////////////////////////////////
+    public static void printArr(int[] arr) {
+        System.out.print("|");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(String.format("%d|", arr[i]));
+        }
+        System.out.println();
     }
 
-    // create left array
-    int larrSize = arr.length / 2;
-    int[] larr = new int[larrSize];
-    for (int i = 0 ; i < arr.length / 2 ; i++) {
-      larr[i] = arr[i];
+    // with int arrays
+    public static int[] sort(int[] arr) {
+
+        //    System.out.println("Sorting");
+
+        if (arr.length <= 1) {
+            return arr;
+        }
+
+        // create left array
+        int larrSize = arr.length / 2;
+        int[] larr = new int[larrSize];
+        for (int i = 0; i < arr.length / 2; i++) {
+            larr[i] = arr[i];
+        }
+
+        // create right array
+        int rarrSize = larrSize;
+        if (arr.length % 2 == 1) {
+            // handle odd number of elements
+            rarrSize++;
+        }
+
+        int[] rarr = new int[rarrSize];
+
+        for (int i = 0; i < rarrSize; i++) {
+            rarr[i] = arr[larrSize + i];
+        }
+
+
+        int[] nlarr = sort(larr);
+        int[] nrarr = sort(rarr);
+
+        int[] result = merge(nlarr, nrarr);
+
+        return result;
+
     }
 
-    // create right array
-    int rarrSize = arr.length / 2;
-    if (arr.length % 2 == 1) {
-      // handle odd number of elements
-      rarrSize = (arr.length / 2) + 1;
-    }
-
-    int[] rarr = new int[rarrSize];
-
-    for (int i = 0 ; i < rarrSize ; i++) {
-      rarr[i] = arr[larrSize + i];
-    }
-
-
-    int[] nlarr = sort(larr);
-    int[] nrarr = sort(rarr);
-
-    int[] result = merge(nlarr, nrarr);
-
-    return result;
-
-  }
-
-  public static int[] merge(int[] larr, int[] rarr) {
+    public static int[] merge(int[] larr, int[] rarr) {
 
     /*
     System.out.println("Merging");
@@ -62,59 +67,152 @@ public class MergeSort {
     printArr(rarr);
     */
 
-    int[] result = new int[larr.length + rarr.length];
+        int[] result = new int[larr.length + rarr.length];
 
-    int l = 0;
-    int r = 0;
-    int i = 0;
+        int l = 0;
+        int r = 0;
+        int i = 0;
 
-    while (l < larr.length || r < rarr.length) {
-      if (l < larr.length && r < rarr.length) {
-        // both have remaining elements
-        if (larr[l] <= rarr[r]) {
-          // choose left
-          result[i] = larr[l];
-          l++;
-        } else {
-          // choose right
-          result[i] = rarr[r];
-          r++;
+        while (l < larr.length || r < rarr.length) {
+            if (l < larr.length && r < rarr.length) {
+                // both have remaining elements
+                if (larr[l] <= rarr[r]) {
+                    // choose left
+                    result[i] = larr[l];
+                    l++;
+                } else {
+                    // choose right
+                    result[i] = rarr[r];
+                    r++;
+                }
+            } else if (l < larr.length) {
+                // choose left
+                result[i] = larr[l];
+                l++;
+            } else if (r < rarr.length) {
+                // choose right
+                result[i] = rarr[r];
+                r++;
+            }
+            i++;
         }
-      } else if (l < larr.length) {
-        // choose left
-        result[i] = larr[l];
-        l++;
-      } else if (r < rarr.length) {
-        // choose right
-        result[i] = rarr[r];
-        r++;
-      }
-      i++;
-    }
 
     /*
     System.out.print("Result: ");
     printArr(result);
     */
 
-    return result;
+        return result;
 
-  }
-
-
-  public static boolean isSorted(int[] arr) {
-
-    boolean isSorted = true;
-
-    for (int i = 0 ; i < (arr.length-1) ; i++) {
-      if (arr[i] > arr[i+1]) {
-        isSorted = false;
-        break;
-      }
     }
 
-    return isSorted;
 
-  }
+    public static boolean isSorted(int[] arr) {
+
+        boolean isSorted = true;
+
+        for (int i = 0; i < (arr.length - 1); i++) {
+            if (arr[i] > arr[i + 1]) {
+                isSorted = false;
+                break;
+            }
+        }
+
+        return isSorted;
+
+    }
+
+
+    ///////////////////////////////////////////////
+    //
+    // Comparable array sort
+    //
+    ///////////////////////////////////////////////
+    public static Comparable[] sort(Comparable[] arr) {
+
+        // exit condition
+        if (arr.length <= 1) {
+            return arr;
+        }
+
+        int lsize = arr.length / 2;
+        int rsize = lsize;
+        if (arr.length % 2 == 1) {
+            // we're odd, give the extra to the right
+            rsize++;
+        }
+
+        Comparable[] larr = new Comparable[lsize];
+        for (int i = 0 ; i < lsize ; i++) {
+            larr[i] = arr[i];
+        }
+
+        Comparable[] rarr = new Comparable[rsize];
+        for (int i = 0 ; i < rsize ; i++) {
+            rarr[i] = arr[lsize + i];
+        }
+
+        larr = sort(larr);
+        rarr = sort(rarr);
+
+        return(merge(larr, rarr));
+
+    }
+
+    public static Comparable[] merge(Comparable[] larr, Comparable[] rarr) {
+
+        Comparable[] result = new Comparable[larr.length + rarr.length];
+
+        int l = 0;
+        int r = 0;
+        int i = 0;
+
+        while (l < larr.length || r < rarr.length) {
+            if (l < larr.length && r < rarr.length) {
+                // both have remaining entries
+                if (larr[l].compareTo(rarr[r]) <= 0) {
+                    // take left
+                    result[i++] = larr[l++];
+                } else {
+                    // take right
+                    result[i++] = rarr[r++];
+                }
+            } else if (l < larr.length) {
+                // take left
+                result[i++] = larr[l++];
+            } else if (r < rarr.length) {
+                // take right
+                result[i++] = rarr[r++];
+            }
+        }
+
+        return result;
+
+    }
+
+
+    public static boolean isSorted(Comparable[] arr) {
+
+        boolean isSorted = true;
+        for (int i = 0 ; i < arr.length - 1 ; i++) {
+            if (arr[i].compareTo(arr[i+1]) > 0) {
+                isSorted = false;
+                break;
+            }
+        }
+
+        return isSorted;
+    }
+
+
+    public static void printArr(Comparable[] arr) {
+        System.out.print("|");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(String.format("%s|", arr[i].toString()));
+        }
+        System.out.println();
+    }
 
 }
+
+// vim: set ft=java:ts=4:sw=4:expandtab:
