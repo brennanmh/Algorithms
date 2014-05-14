8be369d674579ee9260b31c800448a0ff50775fd
Brennan Hildebrand <brenmh@med.umich.edu>
2014-05-14 11:58:55 -0400

    Added sorting project. Just a hello world now. Added xcode C project for algorithms in C.

---
+++ Sorting/src/main/java/org/nthroot/sorting/App.java
@@ -0,0 +1,10 @@
+package org.nthroot.sorting;
+
+
+public class App {
+
+  public static void main(String [] args) {
+
+    System.out.println("Welcome to Sorting!");
+  }
+}
---
+++ algo/algo/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  algo
+//
+//  Created by Brennan Hildebrand on 4/23/14.
+//  Copyright (c) 2014 NthRoot. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
